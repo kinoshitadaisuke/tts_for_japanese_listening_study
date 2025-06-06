6ace64ba61df281a874b0e413c1cf4aa095e3cfc
Daisuke <mhdf23888@gmail.com>
2025-06-06 21:40:17 +0800

    adding "make_mp3_from_mainichi_yoroku.py" on  6/Jun/2025

--- make_mp3_from_mainichi_yoroku.py
+++ make_mp3_from_mainichi_yoroku.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/06/06 12:47:36 (UT+08:00) daisuke>
+# Time-stamp: <2025/06/06 21:37:10 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -16,8 +16,8 @@ import urllib.request
 # importing beautiful soup module
 import bs4
 
-# importing subprocess module
-import subprocess
+# importing edge_tts module
+import edge_tts
 
 # initialising a parser
 descr  = f'Downloading Mainichi Yoroku and generating MP3 file'
@@ -74,14 +74,17 @@ html_article_body  = soup.find ('section', class_='articledetail-body').find ('p
 # converting HTML into plain text
 text_article_title = html_article_title.get_text ()
 text_article_body  = html_article_body.get_text ()
+
+# replacing ▲ with '。\n\n'
 text_article_body  = text_article_body.replace ('▲', '。\n\n')
+
+# combining title and body
+text_article = f'{text_article_title}\n\n{text_article_body}'
 
 # writing extracted plain text into a file
 with open (file_text, 'w') as fh_out:
-    fh_out.write (text_article_title)
-    fh_out.write (f'\n\n')
-    fh_out.write (text_article_body)
+    fh_out.write (text_article)
 
-# executing edge-tts command to make MP3 file
-command_create_mp3 = f'{command_edgetts} -f {file_text} --write-media {file_audio} --voice {voice_name}'
-subprocess.run (command_create_mp3, shell=True)
+# making MP3 file
+communicate = edge_tts.Communicate (text_article, voice_name)
+communicate.save_sync (file_audio)
